92ccdb068c9c22d39381fc61be1968656dbc6472
Ismael Hernandez Munoz <ismael.hernandezm@outlook.com>
2021-02-24 13:07:09 +0100

    created scopedFragment class for local scope

--- app/src/main/java/com/example/tiempo/ui/hoy/CurrentWeatherFragment.kt
+++ app/src/main/java/com/example/tiempo/ui/hoy/CurrentWeatherFragment.kt
@@ -11,13 +11,18 @@ import com.example.tiempo.R
 import com.example.tiempo.data.network.ConnectivityInterceptor
 import com.example.tiempo.data.network.WeatherNetworkDataSourceImpl
 import com.example.tiempo.data.network.WeatherService
+import com.example.tiempo.ui.base.ScopedFragment
 import kotlinx.android.synthetic.main.fragment_current_weather.*
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import org.kodein.di.Kodein
+import org.kodein.di.KodeinAware
+import org.kodein.di.android.x.closestKodein
 
-class CurrentWeatherFragment : Fragment() {
+class CurrentWeatherFragment : ScopedFragment(), KodeinAware     {
 
     private lateinit var viewModel: CurrentWeatherViewModel
+    override val kodein by closestKodein()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -43,6 +48,7 @@ class CurrentWeatherFragment : Fragment() {
         }
 
     }
+
 
 
 
